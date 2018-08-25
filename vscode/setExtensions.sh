219e21ad7b4346416831da4d6e5bc462f0f67c91
DarFig <log.Dariel@gmail.com>
2018-08-25 12:14:14 +0200

    add vscode

---
+++ vscode/setExtensions.sh
@@ -0,0 +1,6 @@
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension dracula-theme.theme-dracula
+code --install-extension MS-CEINTL.vscode-language-pack-es
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension naumovs.color-highlight
---
+++ vscode/vscode.md
@@ -0,0 +1,5 @@
+# VSCODE
+
+- Colocar los archivos de *config* en .config/Code/User/
+- getExtensions.sh para obtener las extensiones instaladas
+- setExtensions.sh para instalar las extensiones en nuestro editor
