49df27face4d76362bb3368b2f6fb389746b1b5b
DarFig <log.Dariel@gmail.com>
2018-08-26 21:04:16 +0200

    add file-icons and panda to vscode

--- vscode/setExtensions.sh
+++ vscode/setExtensions.sh
@@ -1,6 +1,8 @@
 code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension dracula-theme.theme-dracula
+code --install-extension file-icons.file-icons
 code --install-extension MS-CEINTL.vscode-language-pack-es
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
 code --install-extension naumovs.color-highlight
+code --install-extension tinkertrain.theme-panda
