20db01ba7f336066b6b0daee2f41860036003425
DarFig <log.dariel@gmail.com>
2019-02-21 23:29:29 +0100

    upd run.sh

--- run.sh
+++ run.sh
@@ -14,20 +14,38 @@
 if [ $1 = 'spacevim' ]
 then
   echo 'spacevim'
+  which apt >/dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    sudo apt-get install vim
+  fi
+  which pacman >/dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    sudo pacman -S vim
+  else
+    echo 'nada'
+  fi
   curl -sLf https://spacevim.org/install.sh | bash
+  vim
   cp ./spacevim/init.toml ~/.SpaceVim.d/init.toml
 elif [ $1 = 'zsh' ]
 then
   echo 'zsh'
-  if $(apt)
+  which apt >/dev/null 2>&1
+  if [ $? -eq 0 ]
   then
     sudo apt-get install zsh
+    SHELL=$(which zsh)
     chsh -s $(which zsh)
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     cp ./theme.zsh-theme ~/.oh-my-zsh/themes/theme.zsh-theme
-  elif $(pacman)
+  fi
+  which pacman >/dev/null 2>&1
+  if [ $? -eq 0 ]
   then
     sudo pacman -S zsh
+    SHELL=$(which zsh)
     chsh -s $(which zsh)
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     cp ./theme.zsh-theme ~/.oh-my-zsh/themes/theme.zsh-theme
